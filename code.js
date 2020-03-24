72debeeaf3508e42e04fb87feb4645bf7ab7c15f
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-03-24 13:03:17 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,9 +2,9 @@ var curPage = 0,
     correct = 0;
 var myAnswers = [];
 var myQuiz = [
-    ["What is addEventListener() used for?", 1, "attach a click event", "nothing", "never use it", "listens to HTML"],
-    ["What does DOM stand for", 1, "Document Object Model ", "Document Over Mountains", "Do Over Models", "Nothing"],
-    ["What does BOM stand for", 4, "document Object Model", "nothing", "Big Object Model", " Browser Object Model "]
+    ["What is addEventListener() used for?", 1, "attach a click event", "nothing", "never use it", "listens to HTML",false],
+    ["What does DOM stand for", 1, "Document Object Model ", "Document Over Mountains", "Do Over Models", "Nothing",false],
+    ["What does BOM stand for", 4, "document Object Model", "nothing", "Big Object Model", " Browser Object Model ",false]
 ];
 var myHeader = document.getElementById("quizHeader");
 var classname = document.getElementsByClassName("answer");
@@ -16,7 +16,10 @@ var btnFinish = document.getElementById("finishQuiz");
 checkPage();
 btnNext.addEventListener("click", moveNext);
 btnPrevious.addEventListener("click", moveBack);
-btnFinish.addEventListener("click", endQuiz);
+btnFinish.addEventListener("click", check);
+btnYes.addEventListener("click",endQuiz);
+btnNo.addEventListener("click",reload);
+
 for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', myAnswer, false);
 }
@@ -25,29 +28,65 @@ function myAnswer() {
     var idAnswer = this.getAttribute("data-id");
     /// check for correct answer
     myAnswers[curPage] = idAnswer;
-    if (myQuiz[curPage][1] == idAnswer) {
-        //console.log('Correct Answer');
-    } else {
-        //console.log('Wrong Answer');
+   // addBox();
+    if (myQuiz[curPage][1] == idAnswer && !myQuiz[curPage][6]) {
+        console.log('Correct Answer');
+        addBoxCorrect();
+        myQuiz[curPage][6]=true;
+    } else if(!myQuiz[curPage][6]){
+        console.log('Wrong Answer');
+        addBoxWrong();
+        myQuiz[curPage][6]=true;
     }
-    addBox();
+    
 }
-
 function addBox() {
     for (var i = 0; i < myQuestion.children.length; i++) {
         var curNode = myQuestion.children[i];
-        if (myAnswers[curPage] == (i + 1)) {
-            curNode.classList.add("selAnswer");
+        if (myAnswers[curPage] == (i + 1) ) {
+            if(!myQuiz[curPage][6]){
+                console.log("adedPink");
+            curNode.classList.add("selAnswer");}
         } else {
+            console.log("removedPink");
             curNode.classList.remove("selAnswer");
+        }
+    }
+}
+function addBoxCorrect() {
+    for (var i = 0; i < myQuestion.children.length; i++) {
+        var curNode = myQuestion.children[i];
+        if (myAnswers[curPage] == (i + 1) ) {
+            if(!myQuiz[curPage][6]){
+                console.log("addedGreen");
+            curNode.classList.add("greenBox");}
+        } else {
+            console.log("removedGreen");
+            curNode.classList.remove("greenBox");
+        }
+    }
+}
+
+function addBoxWrong() {
+    for (var i = 0; i < myQuestion.children.length; i++) {
+        var curNode = myQuestion.children[i];
+        if (myAnswers[curPage] == (i + 1)  ) {
+            if(!myQuiz[curPage][6]){
+                console.log("addedRed");
+            curNode.classList.add("redBox");
+            }
+        } else {
+            console.log("removedRed");
+            curNode.classList.remove("redBox");
         }
     }
 }
 
 function moveNext() {
     ///check if an answer has been made
-    if (myAnswers[curPage]) {
+   // if (myAnswers[curPage]) {
         //console.log('okay to proceed');
+        //if(!myAnswers[curPage]){iunanswered++;}
         if (curPage < (myQuiz.length - 1)) {
             curPage++;
             checkPage(curPage);
@@ -55,38 +94,64 @@ function moveNext() {
             ///check if quiz is completed
             //console.log(curPage + ' ' + myQuiz.length);
             if (myQuiz.length >= curPage) {
-                endQuiz();
+                ckeck();
+               // endQuiz();
             } else {
                 //console.log('end of quiz Page ' + curPage);
             }
         }
-    } else {
+   // } else {
         //console.log('not answered');
-    }
+    //}
 }
+function check(){
+    if(myAnswers.length !=myQuiz.length){
+        //output='<p style="text-align:center;font-size:2.5em">You have unanswered questions.<br>Are you sure you want to finish?</p>';
+        //document.getElementById("quizContent").innerHTML = output;
+        finishing.classList.remove("hide");
+        btnYes.classList.remove("hide");
+        btnNo.classList.remove("hide");
+        quizContent.classList.add("hide");
 
+    }
+    else{endQuiz();}
+}
+function reload(){
+    curPage=0;
+    console.log(curPage);
+        finishing.classList.add("hide");
+        btnYes.classList.add("hide");
+        btnNo.classList.add("hide");
+        quizContent.classList.remove("hide");
+    checkPage(curPage);
+}
 function endQuiz() {
-    if (myAnswers[2]) {
-        var output = "<div class='output'>Quiz Results<BR>";
+    //if (myAnswers[2]) {
+        btnYes.classList.add("hide");
+        btnNo.classList.add("hide");
+        finishing.classList.add("hide");
+        quizContent.classList.remove("hide");
+        var output = "<div class='output'>Quiz Results<br>";
         var questionResult = "NA";
-        //console.log('Quiz Over');
-        for (var i = 0; i < myAnswers.length; i++) {
+        console.log('Quiz Over');
+        for (var i = 0; i < myQuiz.length; i++) {
             if (myQuiz[i][1] == myAnswers[i]) {
                 questionResult = '<span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span>';
                 correct++;
-            } else {
+            } else if(myQuiz[i][1]!=myAnswers[i] && myAnswers[i]){
                 questionResult = '<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>';
-            }
+            }else{questionResult = '<span>Not Answered</span>'}
             output = output + '<p>Question ' + (i + 1) + ' ' + questionResult + '</p> ';
         }
-        output = output + '<p>You scored ' + correct + ' out of ' + myQuiz.length + '</p></div> ';
+        output = output + '<p><br>You scored ' + correct + ' out of ' + myQuiz.length + '</p></div> ';
         document.getElementById("quizContent").innerHTML = output;
-    } else {
+    //} else {
         //console.log('not answered');
-    }
+   // }
 }
 
 function checkPage(i) {
+    console.log("checkpage")
     /// add remove disabled buttons if there are no more questions in que
     if (curPage == 0) {
         btnPrevious.classList.add("hide");
@@ -104,11 +169,21 @@ function checkPage(i) {
         var curNode = myQuestion.children[i];
         curNode.childNodes[1].innerHTML = capitalise(myQuiz[curPage][(i + 2)]);
         //check if answered already
-        if (myAnswers[curPage] == (i + 1)) {
-            curNode.classList.add("selAnswer");
-            
+        console.log(myAnswers[curPage-1]);
+        if (myAnswers[curPage] == (i + 1) ) {
+            if(myQuiz[curPage][1] == myAnswers[curPage]){
+                curNode.classList.add("greenBox");
+            }
+            else if(myQuiz[curPage][1] != myAnswers[curPage] && myAnswers[curPage]){
+                curNode.classList.add("redBox");
+            }
+            //curNode.classList.add("selAnswer");
+            //console.log("yes");
         } else {
-            curNode.classList.remove("selAnswer");
+            //curNode.classList.remove("selAnswer");
+            curNode.classList.remove("greenBox");
+            curNode.classList.remove("redBox");
+            //console.log('no');
         }
     }
     ///update progress bar
@@ -120,6 +195,7 @@ function checkPage(i) {
 function moveBack() {
     if (curPage > 0) {
         curPage--;
+        console.log(curPage)
         checkPage(curPage);
     } else {
         //console.log('end of quiz Page ' + curPage);
