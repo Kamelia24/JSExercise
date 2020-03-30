488fba2666b4c057e610275b31709e58f8533d27
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-03-30 13:42:18 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+var myQuiz = [
+    question1={
+        question:"What is addEventListener() used for?",
+        answers:["attach a click event","nothing","never use it","listens to HTML"],
+        correct:"attach a click event"
+    },
+    question1={
+        question:"What does DOM stand for?",
+        answers:["Document Over Mountains","Document Object Model","Do Over Models","Nothing"],
+        correct:"Document Object Model"
+    },
+    question1={
+        question:"What does BOM stand for?",
+        answers:["document Object Model","nothing","Big Object Model","Browser Object Model"],
+        correct:"Browser Object Model"
+    }
+]
