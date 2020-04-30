fb64046d30773d408d8e43bf62ffb939a2282839
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-04-30 15:51:46 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser= require("body-parser");
-const Data=require('./data');
+const Data=require('./data.js');
 const quizAll=Data.getQuestions();
 const categories=Data.getCategories();
 const difficulties=Data.getDifficulties();
@@ -24,12 +24,31 @@ app.all('/sort', function(req, res, next) {
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next()
     });
+app.all('/quizQuestions', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next()
+});
+app.all('/newQuestion', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+app.all('/deleteQuestion', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
 app.get("/category",(req, res) => {
         return res.send(categories);
     
 });
 app.get("/difficulty",(req, res) => {
     return res.json(difficulties);
+
+});
+app.get("/quizQuestions",(req, res) => {
+    return res.json(quizAll);
 
 });
 app.post("/sort",(req, res) => {
@@ -44,6 +63,14 @@ app.post("/sort",(req, res) => {
     }
     console.log(sorted)
     res.send(sorted);
+});
+app.post('/newQuestion', (req, res) => {
+    console.log(req.body);
+    Data.log(req.body);
+});
+app.post('/deleteQuestion', (req, res) => {
+    //console.log(req.body);
+    Data.log(req.body);
 });
 var port=3000;
 app.listen(port,()=>{console.log(`Node JS API is listening on port: ${port}`);});
