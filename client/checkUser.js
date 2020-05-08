62f63b5743f6ea8dcb46f14427011c425797831a
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-05-08 11:19:11 +0300

    Update checkUser.js

--- client/checkUser.js
+++ client/checkUser.js
@@ -2,6 +2,35 @@ var reveal=document.getElementById("checkScore");
 reveal.addEventListener("click",showInputField);
 function showInputField(){
     document.getElementById("users").classList.remove("hide");
+    var user=document.getElementById("username").value;
+    console.log(user);
+    $.ajax({
+        url: "http://localhost:3000/showPrevious",
+        type:   'POST',
+        data: {'name': user} ,
+        success: function(msg){
+            console.log(msg);
+            var output="";
+            for(var key in msg){
+                if(key=="empty"){
+                     output='<div>You are new here,better start the quiz. :) </div>';
+                    document.getElementById("showResult").innerHTML=output;
+                    document.getElementById("showResult").classList.remove("hide");
+                }else{
+                    for(let i=1;i<msg.length;i++){
+                     output+=`<div>You scored:${msg[i].score} on ${msg[i].date} at the ${msg[i].difficulty} ${msg[i].category} quiz</div><br>`;
+                    }
+                    document.getElementById("showResult").innerHTML=output;
+                    document.getElementById("showResult").classList.remove("hide");
+                    break;
+                }
+            }
+        },
+        error: function() {
+            $("#failed").css("visibility","visible")
+            //document.getElementById("SubmitButton").disabled = false;
+        }
+    });
 }
 $('#users').on('submit',function(event){
     event.preventDefault();
