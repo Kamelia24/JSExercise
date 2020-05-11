099ea0094e97201e630bcc9a19e90a7121e41ac1
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-05-11 13:45:16 +0300

    Create data.js

---
+++ SameButWithEJS/data/data.js
@@ -0,0 +1,98 @@
+var myQuizАll = [
+    question1={
+        question:"What is addEventListener() used for?",
+        answers:["attach a click event","nothing","never use it","listens to HTML"],
+        correct:"attach a click event",
+        category:"javascript",
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does DOM stand for?",
+        answers:["Document Over Mountains","Document Object Model","Do Over Models","Nothing"],
+        correct:"Document Object Model",
+        category:"javascript",
+        difficulty:"easy"
+    },
+    question1={
+        question:"What ?",
+        answers:["document  Model","nothing","Big  Model","Browser Model"],
+        correct:"Browser Object Model",
+        category:'javascript',
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does ?",
+        answers:[" Model","nothing","Big ","Browser Object Model"],
+        correct:"Browser Object Model",
+        category:'javascript',
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does  for?",
+        answers:["document Object","nothing","Object Model","Browser Object Model"],
+        correct:"Browser Object Model",
+        category:'javascript',
+        difficulty:"easy"
+    },
+    question1={
+        question:"What is HTML?",
+        answers:["Hyper Text Markup Language","nothing","hig tbject Model lyrics","Browser Object Model"],
+        correct:"Hyper Text Markup Language",
+        category:'html',
+        difficulty:'difficult'
+    },
+    question1={
+        question:"What does script stand for?",
+        answers:["tag","nothing","Big Object Model","Browser Object Model"],
+        correct:"tag",
+        category:'html',
+        difficulty:'easy'
+    },
+    question1={
+        question:"What does class stand for?",
+        answers:["class","nothing","Big Object Model","Browser Object Model"],
+        correct:"class",
+        category:"html",
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does text stand for?",
+        answers:["text","nothing","Big Object Model","Browser Object Model"],
+        correct:"text",
+        category:"css",
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does BOM stand for?",
+        answers:["document Object Model","nothing","Big Object Model","Browser Object Model"],
+        correct:"Browser Object Model",
+        category:"css",
+        difficulty:"easy"
+    },
+    question1={
+        question:"What does color stand for?",
+        answers:["color","nothing","Big Object Model","Browser Object Model"],
+        correct:"color",
+        category:'css',
+        difficulty:"difficult"
+    }
+]
+var categories=["html","css","javascript"]
+var difficulties=["easy","difficult"]
+
+module.exports.log=function(msg){
+    if(msg.delete=="delete"){
+        console.log("new array",myQuizАll);
+        myQuizАll.pop();
+        console.log("new array",myQuizАll);
+    }else{
+    myQuizАll.push(msg);
+    }
+    console.log("in");
+}
+const getQuestions=()=>{return myQuizАll;};
+exports.getQuestions=getQuestions;
+const getDifficulties=()=>{return difficulties;};
+exports.getDifficulties=getDifficulties;
+const getCategories=()=>{return categories;};
+exports.getCategories=getCategories;
