ee9b2e4748ca4da0a0228fb0a910c6f679efc83c
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-05-18 09:48:03 +0300

    Create routes.js

---
+++ SameButWithEJS/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller=require('../controllers/controller.js');
+router.get('/',controller.home); 
+router.get('/register',controller.register); 
+router.get('/login',controller.login); 
+router.get('/quiz',controller.getQuiz); 
+router.get('/logOut',controller.logOut);
+router.post("/sort",controller.sortQuiz);
+router.post("/addUser",controller.addUserScore);
+router.post("/addUserData",controller.newUser);
+router.post("/checkUserData",controller.logInVerify);
+module.exports = router;
