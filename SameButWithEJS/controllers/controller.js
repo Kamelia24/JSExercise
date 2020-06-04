04f9e254782cee4267de14d018872c829dd051f8
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-04 16:07:47 +0300

    Update controller.js

--- SameButWithEJS/controllers/controller.js
+++ SameButWithEJS/controllers/controller.js
@@ -1,39 +1,75 @@
 const Data=require('../data/data.js');
-const quizAll=Data.getQuestions();
-const categories=Data.getCategories();
-const difficulties=Data.getDifficulties();
+//const quiz;
+//const answers;
+const quizAll=Data;
+let categories;
+let difficulties;
 const fs=require('fs');
 var rawdata=fs.readFileSync('data/userInfo.json');
 let users=JSON.parse(rawdata);
+const { Client} = require('pg');
+client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    password: '1234',
+	database: 'second',
+	port:5432
+});
+client.connect();
+client.query('SELECT category FROM quiz.categories', (err, res) => {
+    if (err) {
+      console.log (err)
+    }
+    categories=res.rows;
+})
+client.query('SELECT difficulty FROM quiz.difficulties', (err, res) => {
+    if (err) {
+      console.log (err)
+    }
+    difficulties=res.rows;
+})
+client.query(`SELECT question,category,difficulty 
+FROM quiz.question_info inf
+left join quiz.categories cat on inf.category_id=cat.id
+left join quiz.difficulties dif on inf.difficulty_id=dif.id`, (err, res) => {
+    if (err) {
+      console.log (err)
+    }
+    difficulties=res;
+})
 module.exports={
 
     home:function(req, res){ 
+        client.query('SELECT * FROM quiz.users', (err, res) => {
+            if (err) {
+              console.log (err)
+            }
+            console.log(res.rows)
+        });
+        client.query(`SELECT * FROM quiz.quiz_results`, (err, res) => {
+            if (err) {
+              console.log (err)
+            }
+            console.log(res.rows);
+        })
+        client.query('SELECT category FROM quiz.categories', (err, res) => {
+            if (err) {
+              console.log (err)
+            }
+            console.log(res.rows);
+        })
+        client.query(`SELECT question,category,difficulty 
+FROM quiz.question_info inf
+left join quiz.categories cat on inf.category_id=cat.id
+left join quiz.difficulties dif on inf.difficulty_id=dif.id`, (err, res) => {
+    if (err) {
+      console.log (err)
+    }
+    console.log(res.rows);
+})
         res.render('index',{session:req.session.loggedin}); 
     },
-    login:function(req, res){ 
-        if(req.body.session){
-        var data=[];
-        var prof=[];
-        for(key in users){
-            console.log(users[key][0].username,req.session.username)
-            if(users[key][0].username==req.session.username){
-                prof.push(key,users[key][0]);
-                for(let i=1;i<users[key].length;i++){
-                data.push(users[key][i]);
-                }
-                
-                break;
-            }
-        }
-        console.log("profile:",prof,"quiz",data);
-        res.render('profile.ejs',{prof:prof,data:data,session:req.session.loggedin})
-        }else{
-        res.render('sign_in'); 
-        }
-    },
-    register:function(req, res){ 
-        res.render('sign_up'); 
-    },
+    
     getQuiz:function(req, res){ 
         res.render('beginning',{category:categories,difficulty:difficulties,session:req.session.loggedin}); 
     },
@@ -73,71 +109,5 @@ module.exports={
         //console.log(users[`${req.body.user}`][0]);
         let data = JSON.stringify(users);
         fs.writeFileSync('userInfo.json', data);
-    },
-    newUser:function(req,res){
-        console.log("income:",req.body);
-        var hasUser=false;
-        for(key in users){if(key==req.body.user){hasUser=true;}}
-        let data={};
-        username=req.body.username;
-        password=req.body.password;
-        data[`username`]=username;
-        data[`password`]=password;
-        data['age']=req.body.age;
-        console.log("data:",data);
-        if(!hasUser){
-        users[`${req.body.name}`]=[data];
-        }
-        //console.log(users[`${req.body.user}`][0]);
-        let data1 = JSON.stringify(users);
-        fs.writeFileSync('userInfo.json', data1);
-        res.render('sign_in.ejs');
-    },
-    logInVerify:function(req,res){
-        console.log("income:",req.body);
-        let outp={};
-        var hasUser=false;
-        for(key in users){
-    console.log(users[key][0].username,req.body.username)
-                console.log(users[key][0].password,req.body.password)
-            if(users[key][0].username==req.body.username &&
-                req.body.password==users[key][0].password){
-                
-                console.log("correct");
-                req.session.loggedin = true;
-                req.session.username = req.body.username;
-                res.render('beginning.ejs',{category:categories,difficulty:difficulties,session:req.session.loggedin});
-                break;
-        }else{outp="Incorect username or password,please try again!";}
-    }res.send(outp);
-    },
-    logOut:function(req,res,next){
-        if (req.session) {
-            req.session.destroy(function(err) {
-              if(err) {
-                return next(err);
-              } else {
-                return res.redirect('/');
-              }
-            });
-          }
-    },
-    profileInfo:function(req,res){
-        var data=[];
-        var prof=[];
-        for(key in users){
-            console.log(users[key][0].username,req.session.username)
-            if(users[key][0].username==req.session.username){
-                prof.push(key,users[key][0]);
-                for(let i=1;i<users[key].length;i++){
-                data.push(users[key][i]);
-                }
-                
-                break;
-            }
-        }
-        console.log("profile:",prof,"quiz",data);
-        res.render('profile.ejs',{prof:prof,data:data,session:req.session.loggedin})
     }
-
 }
